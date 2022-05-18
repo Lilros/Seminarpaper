11de9a9864b1ad16bdcfbd4d00258ab2ba01aa0c
Lilros <93832109+Lilros@users.noreply.github.com>
2022-05-18 12:01:01 +0200

    Add files via upload

---
+++ seminar project.R
@@ -0,0 +1,11 @@
+library(lavaan)
+if(!require(lavaan)) {
+  install.packages("lavaan"); 
+  require(lavaan)
+}
+
+data<-read.csv("/Users/lillyochsner/seminarprojectusingconditional/refugee_data.csv")
+
+
+
+
